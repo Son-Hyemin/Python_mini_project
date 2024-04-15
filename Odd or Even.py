1a251be9a222fe33f260ff9776973ac3edf45cf7
Son-Hyemin <120477911+Son-Hyemin@users.noreply.github.com>
2024-04-16 02:03:16 +0900

    홀수 짝수 판별기

---
+++ Odd or Even.py
@@ -0,0 +1,9 @@
+
+# 입력한 숫자가 Odd 인지 Even인지 판별하는 프로그램
+
+number = int(input("Which number do you want to check? "))
+
+if number%2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
